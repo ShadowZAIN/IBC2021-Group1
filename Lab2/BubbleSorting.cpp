5f59081cce5de3dd67fa22f54b7a2346f4b4e71d
ShadowZAIN <miroshin.gleb@gmail.com>
2021-08-04 00:06:57 +0300

    Update BubbleSorting.cpp

--- Lab2/BubbleSorting.cpp
+++ Lab2/BubbleSorting.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include <vector>
+#include <chrono>
 
 using namespace std;
 
@@ -7,13 +8,15 @@ int main()
 {
     int n;
     do {
+        cout << "Input array size(positive number): ";
         cin >> n;
-    } while (n < 2);
+    } while (n < 1);
     vector<int> arr;
     arr.resize(n);
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
+    auto start = chrono::high_resolution_clock::now();
     for (int i = 0; i < n - 1; i++)
         for (int j = 0; j < n - i - 1; j++)
             if (arr[j] > arr[j + 1]) {
@@ -21,6 +24,10 @@ int main()
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
             }
-    for (int i = 0; i < n; i++)
-        cout << arr[i] << " ";
+    auto end = chrono::high_resolution_clock::now();
+    chrono::nanoseconds duration = chrono::duration_cast<chrono::nanoseconds>(end - start);
+    cout << "Time taken: " << duration.count() << "ns" << endl;
+    for (int i : arr) {
+        cout << i << " ";
+    }
 }
